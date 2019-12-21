90a28242183901825164a1146a00ce2f12202144
mrater <31857732+mrater@users.noreply.github.com>
2019-12-21 23:02:15 +0100

    added quotes tab

---
+++ generator_core.js
@@ -0,0 +1,7 @@
+//quotes of the Janusz Korwin Mikke
+tab_a = ['Ja chcę powiedzieć jedną rzecz:']
+tab_b = ['przedstawiciele czerwonej hołoty']
+tab_c = ['zupełnie bezkarnie']
+tab_d = ['nawołują do podniesienia podatków']
+tab_e = ['bo dzięki temu mogą kraść']
+tab_f = ['przez kolejne kadencje']
